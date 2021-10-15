123e235c3e108c3610f9b309094fd2827a8318b1
JabezSanjay <jbzsanjay2000@gmail.com>
2021-10-15 21:06:33 +0530

    Basic routing completed

--- letz_collab/webpages/views.py
+++ letz_collab/webpages/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'webpages/home.html')
+
+
+def about(request):
+    return render(request, 'webpages/about.html')
+
+
+def services(request):
+    return render(request, 'webpages/services.html')
+
+
+def contact(request):
+    return render(request, 'webpages/contact.html')
